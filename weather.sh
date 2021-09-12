57eacff003b55e4aefc65fe3377282785068ef33
viksri98 <viksri98@gmail.com>
2021-09-12 16:22:00 -0700

    moved weather.txt to .local/i3blocks/

--- weather.sh
+++ weather.sh
@@ -2,4 +2,6 @@
 
 #Get the temperature, wind, and precipitation
 DATA=$(curl -Ss "http://wttr.in?O&T&Q" | cut -c 16- | head -5 | sed -n '2p;5p;s/\n/\t/;s/~F~Q//')
-echo $DATA > weather.txt
+
+[ -d ~/.local/i3blocks/ ] || mkdir ~/.local/i3blocks
+echo $DATA > ~/.local/i3blocks/weather.txt
